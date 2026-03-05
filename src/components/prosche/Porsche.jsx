d086df19861ed37fe829bfcc25d12b3768b0d7ea
Yusuf <khaidarshikov@gmail.com>
2026-03-05 16:07:23 +0500

    update

--- src/components/prosche/Porsche.jsx
+++ src/components/prosche/Porsche.jsx
@@ -6,9 +6,9 @@ const Porsche = () => {
             <div className="container">
                 <h2 className="porsche_title">GALLERY</h2>
                 <div className="porsche_box">
-                    <img src="./src/images/11.png" alt="" />
-                    <img src="./src/images/22.png" alt="" />
-                    <img src="./src/images/33.png" alt="" />
+                    <img src="../../images/11.png" alt="" />
+                    <img src="../../images/22.png" alt="" />
+                    <img src="../../images/33.png" alt="" />
                 </div>
             </div>
         </div>
