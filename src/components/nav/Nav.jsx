a3c455b8e1506113fece3895b2cb1fef710cb587
Yusuf <khaidarshikov@gmail.com>
2026-03-05 16:34:08 +0500

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
             <div className="container">
                 <div className="nav_box">
                     <a href="#!" className="nav_logo">
-                        <img src="/logo.png" alt="" />
+                        <img src="./logo.png" alt="" />
                     </a>
 
                     <ul className="nav_list">
