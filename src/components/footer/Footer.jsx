ca49daaa99b2d15e0f4bd22e3edc52b643cf3bb4
Yusuf <khaidarshikov@gmail.com>
2026-03-13 15:04:43 +0500

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./footer.scss"
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa6";
 const Footer = () => {
     return (
         <div className="footer">
@@ -14,15 +17,21 @@ const Footer = () => {
                     <div className="footer_right">
                         <ul className="footer_list">
                             <li><h2 className="footer_title">CONTACT</h2></li>
-                            <li><a href="" className="footer_list_link">998 98 888 98 98</a></li>
-                            <li><a href="" className="footer_list_link">porshe@gmail.com</a></li>
-                            <li><a href="" className="footer_list_link">zipcode 32323232</a></li>
+                            <div className="footer_listt">
+                                <li><a href="#!" className="footer_list_link">998 98 888 98 98</a></li>
+                                <li><a href="#!" className="footer_list_link">porshe@gmail.com</a></li>
+                                <li><a href="#!" className="footer_list_link">zipcode 32323232</a></li>
+                            </div>
+
                         </ul>
                         <ul className="footer_list">
                             <li><h2 className="footer_title">SOCIAL</h2></li>
-                            <li><a href="" className="footer_list_link">Instagram</a></li>
-                            <li><a href="" className="footer_list_link">Facebook</a></li>
-                            <li><a href="" className="footer_list_link">Telegram</a></li>
+                            <div className="footer_listt">
+                                <li><a href="#!" className="footer_list_link">Instagram<FaInstagram /></a></li>
+                                <li><a href="#!" className="footer_list_link">Facebook<FaFacebook /></a></li>
+                                <li><a href="#!" className="footer_list_link">Telegram<FaTelegram /></a></li>
+                            </div>
+
                         </ul>
                     </div>
                 </div>
